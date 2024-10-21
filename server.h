8df812328f9750db5aeecac460f70031523f5e28
Vladimir Smetana <smetanavladimir7@gmail.com>
2024-10-21 12:05:50 +0400

    structure

--- server.h
+++
@@ -1,9 +0,0 @@
-
-#include "socket.h"
-class Server : public Socket
-{
-public:
-    explicit Server(int port_number) : Socket(port_number) {};
-    void send_data(int new_socket) override;
-    int getcurtime(char *buffer, size_t bufferSize);
-};
